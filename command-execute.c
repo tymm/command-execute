3c502dc9d3d5d35827248e0b2ab4f4d10c706715
Timm <tymm@gmx.de>
2013-04-30 11:41:58 +0200

    Users can choose now between updates-on-every-IM and updates-only-on-new-IM

--- command-execute.c
+++ command-execute.c
@@ -41,40 +41,61 @@ typedef enum
 } PidginUnseenState;
 #endif
 
-/* cmdexe_conversation_updated is called everytime a message is received */
-static void cmdexe_conversation_updated(PurpleConversation *conv, PurpleConvUpdateType type) {
-	gboolean has_unseen_state=FALSE, has_unseen_count;
-	if( type != PURPLE_CONV_UPDATE_UNSEEN) return;
-
-	/* Due to the really bad API documentation I dont really know whats the difference between unseen-state and unseen-count */
-	//has_unseen_state = (GPOINTER_TO_INT(purple_conversation_get_data(conv, "unseen-state")) >= PIDGIN_UNSEEN_TEXT);
-	has_unseen_count = (GPOINTER_TO_INT(purple_conversation_get_data(conv, "unseen-count")) > 0);
-
-	if(has_unseen_state || has_unseen_count) {
-		const char *cmd = purple_prefs_get_string("/plugins/core/tymm-command-execute/command");
-
-		if(strcmp(cmd,"") != 0) {
-			/* Execute command */
-			if(system(cmd) == 0) {
-				purple_debug_info(PLUGIN_ID, "command executed\n");
-			} else {
-				purple_debug_warning(PLUGIN_ID, "executing the command failed\n");
-			}
+void execute(const char *cmd) {
+	if(strcmp(cmd,"") != 0) {
+		/* Execute command */
+		if(system(cmd) == 0) {
+			purple_debug_info(PLUGIN_ID, "Command executed\n");
 		} else {
-			purple_debug_warning(PLUGIN_ID, "no command found\n");
+			purple_debug_warning(PLUGIN_ID, "There was a problem executing the command\n");
+		}
+	} else {
+		purple_debug_warning(PLUGIN_ID, "No command found\n");
+	}
+}
+
+
+static void cmdexe_conversation_updated(PurpleConversation *conv, PurpleConvUpdateType type) {
+	/* Check if the user wants to execute the command only when a conversation is updated */
+	if(!purple_prefs_get_bool("/plugins/core/tymm-command-execute/execute_always")) {
+		gboolean has_unseen_state=FALSE, has_unseen_count;
+		if( type != PURPLE_CONV_UPDATE_UNSEEN) return;
+
+		//has_unseen_state = (GPOINTER_TO_INT(purple_conversation_get_data(conv, "unseen-state")) >= PIDGIN_UNSEEN_TEXT);
+		has_unseen_count = (GPOINTER_TO_INT(purple_conversation_get_data(conv, "unseen-count")) > 0);
+
+		/* Check if the conversation_updated signal has been emitted by a new message or something else */
+		if(has_unseen_state || has_unseen_count) {
+			const char *cmd = purple_prefs_get_string("/plugins/core/tymm-command-execute/command");
+			execute(cmd);
 		}
 	}
 }
 
+static void cmdexe_received_im_msg(PurpleConversation *conv, PurpleConvUpdateType type) {
+	/* Check if the user wants to execute the command on _every_ received IM */
+	if(purple_prefs_get_bool("/plugins/core/tymm-command-execute/execute_always")) {
+		const char *cmd = purple_prefs_get_string("/plugins/core/tymm-command-execute/command");
+		execute(cmd);
+	}
+}
+
 static gboolean plugin_load(PurplePlugin *plugin) {
-	/* Connect the conversation-updated signal to the callback function cmdexe_conversation_updated */
+	/* Connect the conversation-updated signal to the callback function cmdexe_conversation_updated.
+	 * The conversation-updated signal is emitted when a conversation is updated.
+	 * I discovered that the conversation-updated signal is also triggered if the focus of a chat window is being triggered */
 	purple_signal_connect(purple_conversations_get_handle(), "conversation-updated", plugin, PURPLE_CALLBACK(cmdexe_conversation_updated), NULL);
+
+	/* Connect the received-im-msg signal to the callback function cmdexe_received_im_msg.
+	 * This signal is emitted everytime an IM message is received */
+	purple_signal_connect(purple_conversations_get_handle(), "received-im-msg", plugin, PURPLE_CALLBACK(cmdexe_received_im_msg), NULL);
 	return TRUE;
 }
 
 static gboolean plugin_unload(PurplePlugin *plugin) {
 	/* Disconnect the signal if the plugin is getting unloaded */
 	purple_signal_disconnect(purple_conversations_get_handle(), "conversation-updated", plugin, PURPLE_CALLBACK(cmdexe_conversation_updated));
+	purple_signal_disconnect(purple_conversations_get_handle(), "received-im-msg", plugin, PURPLE_CALLBACK(cmdexe_received_im_msg));
 	return TRUE;
 }
 
@@ -89,6 +110,9 @@ static PurplePluginPrefFrame *plugin_config_frame(PurplePlugin *plugin) {
 	purple_plugin_pref_frame_add(frame, ppref);
 
 	ppref = purple_plugin_pref_new_with_name_and_label("/plugins/core/tymm-command-execute/command", "Command");
+	purple_plugin_pref_frame_add(frame, ppref);
+
+	ppref = purple_plugin_pref_new_with_name_and_label("/plugins/core/tymm-command-execute/execute_always", "Execute command on every new IM (not only on updated conversations)");
 	purple_plugin_pref_frame_add(frame, ppref);
 
 	return frame;
@@ -115,11 +139,11 @@ static PurplePluginInfo info = {
 	PURPLE_PRIORITY_DEFAULT,
 	PLUGIN_ID,
 	"Command execute",
-	"0.1",
+	"1.0",
 	"Command execution for pidgin and finch",
-	"Takes a command which will be executed everytime a message is received",
-	"tymm <tymm@gmx.de>",
-	"",
+	"Takes a command which will be executed either on every new IM or on every conversation update",
+	"tymm <tymmm1@gmail.com>",
+	"https://github.com/tymm/command-execute/",
 	plugin_load,
 	plugin_unload,
 	NULL, // plugin_destroy
@@ -136,6 +160,7 @@ static PurplePluginInfo info = {
 static void init_plugin(PurplePlugin *plugin) {
 	purple_prefs_add_none("/plugins/core/tymm-command-execute");
 	purple_prefs_add_string("/plugins/core/tymm-command-execute/command", "");
+	purple_prefs_add_bool("/plugins/core/tymm-command-execute/execute_always", FALSE);
 }
 
 PURPLE_INIT_PLUGIN(command-execute, init_plugin, info)
